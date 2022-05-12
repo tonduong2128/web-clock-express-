f4cf2ee0bd8004913a2c5d5ebabc8db6b1f00a0d
Ton Duong <tonduong2128@gmail.com>
2022-05-12 15:25:06 +0700

    add env

--- src/database/db.mjs
+++ src/database/db.mjs
@@ -1,12 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export default {
-    connect: async () => {
-        try {
-            await mongoose.connect('mongodb://localhost:27017/web-express');
-            console.log("connect database successfully");
-        } catch (error) {
-            console.log("Connect to failed");
-        }
+  connect: async () => {
+    try {
+      await mongoose.connect(process.env.DB);
+      console.log("connect database successfully");
+    } catch (error) {
+      console.log("Connect to failed");
     }
-}
+  },
+};
--- src/index.mjs
+++ src/index.mjs
@@ -1,26 +1,31 @@
 import bodyParser from "body-parser";
-import cookieParser from 'cookie-parser';
-import express from 'express';
-import { engine } from 'express-handlebars';
-import db from './database/db.mjs';
-import routes from './routes/index.mjs';
-import functionHelper from './util/functionHelper.mjs';
+import cookieParser from "cookie-parser";
+import express from "express";
+import { engine } from "express-handlebars";
+import db from "./database/db.mjs";
+import routes from "./routes/index.mjs";
+import functionHelper from "./util/functionHelper.mjs";
+import dotenv from "dotenv";
 
-const app = express()
+dotenv.config();
+const app = express();
 
 db.connect();
 
-app.use(bodyParser.json()) 
-app.use(bodyParser.urlencoded({ extended: true })) 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cookieParser())
-app.use('/', express.static('./src/views/'));
-app.engine('.hbs', engine({
-    extname: '.hbs',
-    helpers: functionHelper
-}));
-app.set('view engine', '.hbs');
-app.set('views', './src/views');
+app.use(cookieParser());
+app.use("/", express.static("./src/views/"));
+app.engine(
+  ".hbs",
+  engine({
+    extname: ".hbs",
+    helpers: functionHelper,
+  })
+);
+app.set("view engine", ".hbs");
+app.set("views", "./src/views");
 
 routes(app);
 
