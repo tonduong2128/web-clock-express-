e11c4a5268e759ef9f28b0a9f9e1cbcaf14e2d37
Ton Duong <tonduong2128@gmail.com>
2022-05-18 23:53:38 +0700

    up

--- src/routes/homeRoute_fe.mjs
+++ src/routes/homeRoute_fe.mjs
@@ -2,28 +2,28 @@ import express from "express";
 import HomeController from "../controllers/HomeController_fe.mjs";
 const route = express.Router();
 
-// route.get("/dang-nhap", HomeController.dangNhap)
-// route.post("/dang-nhap", HomeController._dangNhap)
+route.get("/dang-nhap", HomeController.dangNhap);
+route.post("/dang-nhap", HomeController._dangNhap);
 
-// route.get("/dang-ky", HomeController.dangKy)
-// route.post("/dang-ky", HomeController._dangKy)
+route.get("/dang-ky", HomeController.dangKy);
+route.post("/dang-ky", HomeController._dangKy);
 
-// route.get("/dang-xuat", HomeController.dangXuat)
+route.get("/dang-xuat", HomeController.dangXuat);
 
-// route.get("/chi-tiet-san-pham", HomeController.chiTiet)
+route.get("/chi-tiet-san-pham", HomeController.chiTiet);
 
-// route.get("/them-san-pham", HomeController.themSanPham)
+route.get("/them-san-pham", HomeController.themSanPham);
 
-// route.get("/gio-hang", HomeController.gioHang)
+route.get("/gio-hang", HomeController.gioHang);
 
-// route.get("/xoa-san-phan", HomeController.xoaSanPham)
+route.get("/xoa-san-phan", HomeController.xoaSanPham);
 
-// route.get("/thong-tin-tai-khoan", HomeController.taiKhoan)
+route.get("/thong-tin-tai-khoan", HomeController.taiKhoan);
 
-// route.post("/thong-tin-tai-khoan", HomeController._taiKhoan)
-// route.get("/dat-hang", HomeController.datHang)
-// route.get("/don-hang", HomeController.donHang)
-// route.get("/da-giao-hang", HomeController.giaoHang)
+route.post("/thong-tin-tai-khoan", HomeController._taiKhoan);
+route.get("/dat-hang", HomeController.datHang);
+route.get("/don-hang", HomeController.donHang);
+route.get("/da-giao-hang", HomeController.giaoHang);
 
 route.use("/", HomeController.index);
 
--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -3,5 +3,5 @@ import homeRoute_be from "./homeRoute_be.mjs";
 
 export default function (app) {
   app.use("/", homeRoute_fe);
-  // app.use("/admin", homeRoute_be);
+  app.use("/admin", homeRoute_be);
 }
