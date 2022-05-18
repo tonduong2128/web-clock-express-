07e1a74923b992c7825d5b6a9809a04c0743348f
Ton Duong <tonduong2128@gmail.com>
2022-05-19 00:31:58 +0700

    up

--- src/database/db.mjs
+++ src/database/db.mjs
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 export default {
   connect: async () => {
     try {
-      await mongoose.connect(process.env.DB);
+      // await mongoose.connect(process.env.DB);
+      await mongoose.connect(
+        "mongodb+srv://tonduong2128:tonduong2128@cluster0.u5ntb.mongodb.net/web-express"
+      );
       console.log("connect database successfully");
     } catch (error) {
       console.log("Connect to failed");
--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -1,7 +1,7 @@
 import homeRoute_fe from "./homeRoute_fe.mjs";
 import homeRoute_be from "./homeRoute_be.mjs";
 
-export default function (app) {
+export default function routes(app) {
   app.use("/", homeRoute_fe);
   app.use("/admin", homeRoute_be);
 }
